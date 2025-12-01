da9a9bdb65bfd6e751549e360746c7dbef551fe5
codearvind07 <arvindfb07@gmail.com>
2025-12-01 14:27:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "REHAN BHAI️™️"; 
+document.getElementById("subscriberCount").innerText = "13,423+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
